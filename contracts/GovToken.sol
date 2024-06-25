815c4ed38ce3c2b5190a65229e6565eb457f04e4
saik434455@gmail.com <saik434455@gmail.com>
2024-06-25 20:36:11 +0530

    Governance token contract

---
+++ contracts/GovToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract MyToken is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner)
+        ERC20("KG_Decentralized_Token", "KGT")
+        Ownable(initialOwner)
+        ERC20Permit("KG_Decentralized_Token")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
